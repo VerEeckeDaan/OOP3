c4472ba19f5316220ec281496f36e269eecb3a91
Daan Ver Eecke <daan.vereecke.dv@gmail.com>
2018-02-19 16:18:40 +0100

    add changes to voorbeeldles

--- voorbeeldles/main.cpp
+++ voorbeeldles/main.cpp
@@ -7,10 +7,13 @@ int main()
     int x = 15;
     int * pX = &x;
 
-    pX = 45;
+    (*pX) = 45;
+
+    int numbers [10];
 
     cout << "address of x = " << &x << endl;
     cout << "address of x = " << pX << endl;
+    cout << "address of numbers = " << numbers;
 
 	return 0;
 }
