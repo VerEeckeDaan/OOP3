86d1f5981527dbaa225f78b5fd47bb42d1aea377
Daan Ver Eecke <daan.vereecke.dv@gmail.com>
2018-02-19 15:03:40 +0100

    add program displaying variable value

---
+++ Displaying variable value/main.cpp
@@ -0,0 +1,16 @@
+//main.cpp
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  int value = 0;
+
+    std::cout << "Value: " << value << endl ;
+    std::cout << "Please enter value: ";
+    std::cin >> value;
+    std::cout << "Value: " << value;
+
+  return 0;
+}
